bda6e0f146182717df3c5108965766c73bb725ba
Dhruveeee26 <Bhaviverma708@gmail.com>
2026-07-05 23:28:21 +0530

    sets

---
+++ sets.py
@@ -0,0 +1,24 @@
+# sets are collection of items
+# sets are unordered, mutable (changeable), and do not allow duplicate elements but only one occurance will be stored in the set.
+# sets are defined by enclosing elements in curly braces {} or by using the set() constructor.
+# example: my_set = {1, 2, 3, "hello", True}
+# indexing in sets is not possible because they are unordered.
+# lengths of sets can be found using the len() function.
+# slicing in sets is not possible because they are unordered.
+# unique elements can be stored in sets, meaning that if you try to add a duplicate element, it will be ignored.
+
+# membership testing: 'in' is used to check if a specified value exists in the set. It returns True if the value is found, otherwise False.
+# not in is used to check if a specified value does not exist in the set.
+# concatenation cannot be done on sets with '+'
+# repetition cannot be done on sets with '*'
+# conversion of list into set can be done using set() function.
+# comversion of tupple into set and vice versa can be done using set() and tuple() functions.
+# sets are mutable, meaning that you can add or remove elements from a set after it has been created.
+# add method is used to add a single element to the set.
+# set.add(element)
+# update method is used to add multiple elements to the set from another iterable (like a list, tuple, or another set).
+# set.update([iterable])
+# remove method is used to remove a specified element from the set. If the element is not found, it raises a KeyError.
+# set.remove(element)
+# discard method is used to remove a specified element from the set. If the element is not found, it does nothing (no error).
+# set.discard(element)
