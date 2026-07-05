77ef9641dcd97a15d5078cbf8958b78187554455
Dhruveeee26 <Bhaviverma708@gmail.com>
2026-07-05 08:57:27 +0530

    day 3

--- list.py
+++ list.py
@@ -5,5 +5,28 @@
 # They are defined by enclosing elements in square brackets [].
 # example: my_list = [1, 2, 3, "hello", True]
 # indexing in lists starts from 0, meaning the first element is at index 0, the second at index 1, and so on.
+
+
+# DAY- 3
+
+
 # slicing in lists allows you to access a range of elements by specifying a start and end index. 
 # The syntax is list[start:end], where the start index is inclusive and the end index is exclusive.
+# concatenation of strings : + is used to combine two or more strings together.
+# print(list1 + list2)
+# repetition of strings : * is used to repeat a string multiple times.
+# print (list2 * 3)
+# append is a method used to add a single element to the end of a list.
+# list.append(element)
+# append function updates the list but does not return any value (returns None).
+# LIST ARE MUTABLE
+# insert adds an element at a specific index in the list.
+# list.insert(index, element)
+# extend adds all elements of an iterable (like another list) to the end of the list.
+# list.extend([iterable])
+# append only adds one element whereas extend can add multiple elements from another iterable.
+# remove method removes the first occurrence of a specified value from the list.
+# list.remove(value)
+# pop method removes and returns the element at a specified index (default is the last element).
+# list.pop(index)
+
