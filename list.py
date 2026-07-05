4add32d3d1e804a8564b8c3dc50435394b474557
Dhruveeee26 <Bhaviverma708@gmail.com>
2026-07-05 10:14:09 +0530

    list completed

--- list.py
+++ list.py
@@ -29,4 +29,29 @@
 # list.remove(value)
 # pop method removes and returns the element at a specified index (default is the last element).
 # list.pop(index)
-
+# reverse method reverses the elements of the list in place.
+# list.reverse()
+# sort method sorts the elements of the list in ascending order by default.
+#  You can also specify a custom sorting order using the key parameter.
+# list.sort()- will sort the list in ascending order by default.
+# list.sort(reverse=True) - will sort the list in descending order.
+# count method returns the number of occurrences of a specified value in the list.
+# list.count(value)
+# in function is used to check if a specified value exists in the list. It returns True if the value is found, otherwise False.
+# value in list
+# print("dhruvi" in list1) - will return True if "dhruvi" is present in list1, otherwise False.
+# not in function is used to check if a specified value does not exist in the list. It returns True if the value is not found, otherwise False.
+# value not in list
+# print("dhruvi" not in list1) - will return True if "dhruvi" is not present in list1, otherwise False.
+# min function is used to find the minimum value in a list of numbers.
+# min(list)
+# print(min(list1)) - will return the minimum value in list1.
+# max function is used to find the maximum value in a list of numbers.
+# max(list)
+# print(max(list1)) - will return the maximum value in list1.
+# sum function is used to calculate the sum of all elements in a list of numbers.
+# sum(list)
+# print(sum(list1)) - will return the sum of all elements in list1.
+# list in a list is a list that contains other lists as its elements.
+# It allows you to create multi-dimensional data structures.
+# l2 = [[1, 2, 3], [4, 5, 6], [7, 8,[0,1]]]
