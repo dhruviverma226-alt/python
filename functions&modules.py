999fce9b1f233e0814b9e3030daf6662ad1adcda
dhruviverma226-alt <dhruviverma226@gmail.com>
2026-07-14 13:49:00 +0530

    func intro

---
+++ functions&modules.py
@@ -0,0 +1,9 @@
+# built in functions- already present functions developed by developers that is present when we download python!!
+# user-defined functions- functions that we create ourselves!!
+#functions are used to perform a specific task!!
+# syntax of function- def function_name(parameters):
+# function_name is the name of the function
+# parameters are the values that we pass to the function
+# def is used to define a function
+# parameters are optional
+# parameters are used to pass values to the function
