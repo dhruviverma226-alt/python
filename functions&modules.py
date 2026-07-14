15e802540b3856afd3492c4d429cc1d798d4b5d5
dhruviverma226-alt <dhruviverma226@gmail.com>
2026-07-14 23:49:19 +0530

    defining func

--- functions&modules.py
+++ functions&modules.py
@@ -1,9 +1,32 @@
 # built in functions- already present functions developed by developers that is present when we download python!!
 # user-defined functions- functions that we create ourselves!!
-#functions are used to perform a specific task!!
+# functions are used to perform a specific task!!
 # syntax of function- def function_name(parameters):
 # function_name is the name of the function
 # parameters are the values that we pass to the function
 # def is used to define a function
 # parameters are optional
 # parameters are used to pass values to the function
+
+"""example:
+def greeting(name):
+    print(f"Hello {name}")
+    print("its a beautiful day")
+
+greeting("Dhruvi")"""
+
+"""def even_odd(number):
+    if number % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+even_odd(10)"""
+# returning values- "none" is returned if the functions stores the value
+
+"""def even_odd(number):
+    if number % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+print(even_odd(10))
+"""
